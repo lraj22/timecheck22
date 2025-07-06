319ac690ce8a6ec8cf3f872c13e7181fb66cd5d8
lraj22 <63673413+lraj22@users.noreply.github.com>
2025-07-07 01:24:11 +0530

    Create basic frame & comments

--- src/helper.js
+++ src/helper.js
@@ -1,1 +1,14 @@
-console.log("hello");
+// helper.js - provides helpful things to other files!
+
+export const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+// keeps track of when page is fully loaded
+let loadFlags = 0;
+export function loaded () {
+	var threshold = 1; // waiting for 1 flag: window.onload
+	if (!navigator.onLine) threshold = 1; // just wait for load
+	loadFlags++;
+	if (loadFlags >= threshold) {
+		console.log("finished loading!");
+	}
+}
--- src/main.js
+++ src/main.js
@@ -1,5 +1,28 @@
+import { loaded, months } from "./helper";
 import "./main.css";
 
+// import all DOM elements by [id]
+var dom = {};
+document.querySelectorAll("[id]").forEach(element => dom[element.id] = element);
+
+// this function runs all the time
+function tick () {
+	let now = new Date();
+	
+	// update clock
+	dom.time.textContent = "" + ((now.getHours() % 12) || 12) + ":" + now.getMinutes();
+	dom.date.textContent = "" + months[now.getMonth()] + " " + now.getDate() + ", " + now.getFullYear();
+	
+	// request next tick
+	requestAnimationFrame(tick);
+}
+
+requestAnimationFrame(tick);
+
+// on page load
+window.addEventListener("load", () => loaded());
+
+// set up sw.js if supported
 if ("serviceWorker" in navigator) {
 	navigator.serviceWorker.register("./sw.js");
 }
