10f5810d561d675d1d7487f161106e7f098a97b8
lraj22 <63673413+lraj22@users.noreply.github.com>
2026-01-02 01:32:49 -0800

    Add events overview to analytics

--- extras/analytics/analyze.py
+++ extras/analytics/analyze.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from scipy import stats
 import country_converter as coco
 import os
 
@@ -25,7 +26,8 @@ sd_df = sd_df.drop(columns=['website_id', 'number_value', 'date_value', 'data_ty
 # Get developer/old sessions
 cond_1 = ((sd_df['data_key'] == 'env') & (sd_df['string_value'] == 'dev')) # env=dev
 cond_2 = (sd_df['data_key'] == 'profile') # profile=[identifier for one of my testing devices]
-dev_session_mask = (cond_1 | cond_2)
+cond_3 = ((sd_df['data_key'] == 'schoolId') & (sd_df['string_value'] == '2') & (sd_df['created_at'] > '2025-12-20') & (sd_df['created_at'] < '2025-12-31')) # no production Cal Aero (schoolId: 2) users during 2025 Dec 20-31, only developers
+dev_session_mask = (cond_1 | cond_2 | cond_3)
 dev_sessions = sd_df[dev_session_mask]
 dev_ids = dev_sessions['session_id'].unique()
 
@@ -44,6 +46,7 @@ bad_we_visits_mask = (
 )
 bad_we_visits = we_df[bad_we_visits_mask]
 bad_we_visit_ids = bad_we_visits['visit_id'].unique()
+bad_we_event_ids = bad_we_visits['event_id'].unique()
 
 # Get real valid user events
 user_we_events = we_df[~we_df['visit_id'].isin(bad_we_visit_ids)]
@@ -53,15 +56,26 @@ ed_df = pd.read_csv(event_data_csv_path, parse_dates=['created_at'])
 ed_df = ed_df.drop(columns=['website_id', 'url_path', 'number_value', 'date_value', 'data_type', 'job_id']) # note to self: remove 'url_path' from drop list when other pages get events on them
 
 # Filter out invalid data
+# old notes-updated events fired too much before a bugfix was committed (de24e77862d4dcc3c2a3340fdb6dabb60e06cacc)
+NOTES_UPDATED_OVERFIRE_FIXED_TS = '2025-12-31 12:15'
 valid_notes_updated_event_ids = ed_df[(
 	(ed_df['event_name'] == 'notes-updated') &
 	(ed_df['data_key'] == 'length')
 )][['session_id', 'event_id', 'string_value']].drop_duplicates(subset=['session_id', 'string_value'])['event_id'].unique()
-user_we_events = user_we_events[~( # old notes-updated events fired too much before a bugfix was committed (de24e77862d4dcc3c2a3340fdb6dabb60e06cacc)
+valid_ed_events = ed_df[~(
+	(ed_df['event_name'] == 'notes-updated') &
+	(ed_df['created_at'] < NOTES_UPDATED_OVERFIRE_FIXED_TS) &
+	(~ed_df['event_id'].isin(valid_notes_updated_event_ids))
+)]
+user_we_events = user_we_events[~(
 	(user_we_events['event_name'] == 'notes-updated') &
-	(user_we_events['created_at'] < '2025-12-31 12:15') &
+	(user_we_events['created_at'] < NOTES_UPDATED_OVERFIRE_FIXED_TS) &
 	(~user_we_events['event_id'].isin(valid_notes_updated_event_ids))
 )]
+# Remove bad events as defined above
+valid_ed_events = valid_ed_events[~valid_ed_events['event_id'].isin(bad_we_event_ids)]
+
+
 
 # Get country names from Umami source data (ISO 3166-1 alpha-2 country codes)
 country_subset = user_we_events[['session_id', 'country']].drop_duplicates()
@@ -80,6 +94,8 @@ Notes:
 	- View = when a page is viewed. Comes under visit, so in one visit, a page can be viewed multiple times
 """
 
+
+
 # Show initial data
 total_user_sessions = len(user_we_events['session_id'].unique())
 visits_df = user_we_events[['session_id', 'visit_id']].drop_duplicates()
@@ -96,6 +112,8 @@ write("Number of real user page visits:", total_user_visits)
 write("Number of real user page views:", total_user_views)
 write("---")
 write()
+
+
 
 # Count number of repeat visitors
 number_of_users = -1
@@ -115,6 +133,8 @@ for amount in repeats:
 	repeat_count += 1
 write()
 
+
+
 # Check what kinds of devices are used
 write("== Devices used ==")
 devices_by_session_id = user_we_events.groupby('session_id')['device'].unique()
@@ -127,6 +147,8 @@ write()
 # write("-- Device usage combos --")
 # for devices, count in devices_by_session_id.value_counts().items():
 # 	write(count, "person uses" if count == 1 else "people use", "the following:", ' & '.join(devices))
+
+
 
 # Check where they came from
 write("== Visitor referrals ==")
@@ -145,10 +167,26 @@ for referrer, count in number_of_users_by_referrer:
 	else:
 		write(count, "user" if count == 1 else "users", "reached TC22 via", referrer_map.get(referrer, referrer))
 write("Note that one user can use visit multiple times, so total may not add to", total_user_sessions, "(total # of users)")
+write()
+
+
+
+# Events overview
+event_names_by_session_id = valid_ed_events.groupby('session_id')['event_name'].count()
+event_names_by_session_id = pd.concat([event_names_by_session_id, pd.Series([0] * (total_user_sessions - len(event_names_by_session_id)))]) # Fill with 0-event users until proper number of users achieved
+avg_events_per_user = round(event_names_by_session_id.mean(), 2)
+percentile_of_avg_events = round(stats.percentileofscore(event_names_by_session_id, event_names_by_session_id.mean()), 2)
+quantile_values = event_names_by_session_id.quantile([0.25, 0.5, 0.75]).tolist()
+write('== Events overview ==')
+write(f'There are {total_user_sessions} users, and on average, each user does about {avg_events_per_user} events. This is more than {percentile_of_avg_events}% of users. 75% of users have at least {quantile_values[0]} events, 50% of users have at least {quantile_values[1]} events, and 25% of users have at least {quantile_values[2]} events. The user with the most events has {event_names_by_session_id.max()} events.')
+
+
 
 # Write to analytics report file
 with open(analytics_report_file_path, 'w') as report_file:
 	report_file.writelines(lines_to_write)
+
+
 
 # Check uniqueness (dev)
 exit(0) # only for developers!
